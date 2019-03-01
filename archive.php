782d174f15a8c53a2104f3eae86decbee4f6719d
Jeremiah <happytobike@gmail.com>
2019-03-01 10:30:20 -0800

    Styling for post archives

--- archive.php
+++ archive.php
@@ -7,35 +7,36 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+    <i class="fas fa-quote-left"></i>
+    <main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+        <?php if (have_posts()): ?>
 
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-				?>
-			</header><!-- .page-header -->
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+        <?php
+        the_archive_title('<h1 class="page-title">', '</h1>');
+        ?>
 
-				<?php
-					get_template_part( 'template-parts/content' );
-				?>
 
-			<?php endwhile; ?>
+        <?php  /* Start the Loop */ ?>
+        <?php while (have_posts()): the_post(); ?>
 
-			<?php the_posts_navigation(); ?>
+        <?php
+        get_template_part('template-parts/content');
+        ?>
 
-		<?php else : ?>
+        <?php endwhile; ?>
 
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+        <?php qod_numbered_pagination(); ?>
 
-		<?php endif; ?>
+        <?php else: ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+        <?php get_template_part('template-parts/content', 'none'); ?>
 
-<?php get_footer(); ?>
+        <?php endif; ?>
+
+    </main><i class="fas fa-quote-right"></i><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?> 
